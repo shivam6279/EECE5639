e7eee500f239d3b68c56ae104a8f98ad6de85e17
Shivam Sharma <shivam6279@gmail.com>
2019-10-19 16:57:22 -0400

    added Harris

--- P2/p2.py
+++ P2/p2.py
@@ -5,6 +5,29 @@ import cv2
 import os 
 import gc
 
+def Harris(img, window_size=3, k=0.04):
+    h, w = img.shape[0], img.shape[1]
+
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+
+    Ixx = sobelx ** 2
+    Iyy = sobely ** 2
+    Ixy = sobelx * sobely
+    R = np.zeros_like(img, dtype=np.float64)
+
+    offset = np.floor(window_size / 2).astype(np.int8)
+    for y in range(offset, h-offset):
+        for x in range(offset, w-offset):
+            Sxx = np.sum(Ixx[y-offset:y+1+offset, x-offset:x+1+offset])
+            Syy = np.sum(Iyy[y-offset:y+1+offset, x-offset:x+1+offset])
+            Sxy = np.sum(Ixy[y-offset:y+1+offset, x-offset:x+1+offset])
+            det = (Sxx * Syy) - (Sxy ** 2)
+            trace2 = (Sxx + Syy) ** 2
+            R[y][x] = det - k*trace2
+        
+    return R
+
 
 
 dataset_path = ["DanaHallWay1"]#, "DanaHallWay2", "DanaOffice"]
