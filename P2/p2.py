de9213d6d191cc1321440f1d194e6bc68f5e8b38
juanpbm <31747012+juanpbm@users.noreply.github.com>
2019-10-19 21:40:13 -0400

    Update p2.py

--- P2/p2.py
+++ P2/p2.py
@@ -5,11 +5,14 @@ import cv2
 import os 
 import gc
 
-def Harris(img, window_size=3, k=0.04):
+def Harris(img, window_size=3, sobel_size=3, k=0.04):
+    if len(img.shape) > 2:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
     h, w = img.shape[0], img.shape[1]
 
-    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
-    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_size)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_size)
 
     Ixx = sobelx ** 2
     Iyy = sobely ** 2
@@ -22,16 +25,61 @@ def Harris(img, window_size=3, k=0.04):
             Sxx = np.sum(Ixx[y-offset:y+1+offset, x-offset:x+1+offset])
             Syy = np.sum(Iyy[y-offset:y+1+offset, x-offset:x+1+offset])
             Sxy = np.sum(Ixy[y-offset:y+1+offset, x-offset:x+1+offset])
-            det = (Sxx * Syy) - (Sxy ** 2)
-            trace2 = (Sxx + Syy) ** 2
-            R[y][x] = det - k*trace2
-        
+            r = (Sxx * Syy) - (Sxy ** 2) - k * (Sxx + Syy) ** 2
+            if r < 0:
+                r = 0
+            R[y][x] = r
+
+    R /= np.max(R)
+    R *= 255.0
+    # _, R = cv2.threshold(R, 0.01 * np.max(R), 255, 0)
     return R
 
 
+def non_max_suppression(img, window_size=5, thresh=2.5):
+    h, w = img.shape[0], img.shape[1]
+
+    offset = np.floor(window_size / 2).astype(np.int8)
+
+    for y in range(h):
+        for x in range(w):
+            if x <= offset or x >= (w - offset) or y <= offset or y >= (h - offset):
+                img[y][x] = 0
+
+    index = np.zeros((h, w, 3), dtype=np.uint16)
+    for y in range(h):
+        for x in range(w):
+            index[y][x] = (img[y][x], x, y)
+
+    corners = []
+
+    for y in range(offset, h - offset, offset):
+        for x in range(offset, w - offset, offset):
+            ret = mean_shift_converge(index, (x, y), window_size, thresh)
+            if ret:
+                corners.append(ret)
+
+    list(set(corners))
+    return corners
+
+def mean_shift_converge(index, point, window_size=5, thresh=2.5):
+    x, y = point
+    offset = np.floor(window_size / 2).astype(np.int8)
+
+    window = index[y - offset:y + 1 + offset, x - offset:x + 1 + offset]
+    if np.max(window[:, :, 0]) > thresh:
+        window = np.reshape(window, (window_size ** 2, 3))
+        window[::-1] = window[window[:, 0].argsort()]
+        if window[0][1] == x and window[0][2] == y:
+            return x, y
+        else:
+            return mean_shift_converge(index, (window[0][1], window[0][2]), window_size, thresh)
+    else:
+        return None
+
+
 def NCC(imgset, corners):
-    h, w = imgset[0].shape[0], imgset[0].shape[1]
-    h1, w1 = imgset[1].shape[0], imgset[1].shape[1]
+
     res = []
     for i in corners[0]:
         if (i[1] != 0 and i[0] != 0 and i[1] != 339 and  i[0] != 511  ):
@@ -85,7 +133,12 @@ for path in dataset_path:
     grayscale = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)for img in imgset]
     grayscale = [np.float32(img) for img in grayscale]
 
-    cor = [cv2.cornerHarris(gray,2,3,0.04) for gray in grayscale]
+    cor = [Harris(gray,3,3,0.4) for gray in grayscale]
+    corners = [non_max_suppression(img,5) for img in cor]
+   
+    corres = NCC(grayscale,corners)
+    print (corres[1][1])
+    '''
     cor = [cv2.dilate(img,None) for img in cor]
 
     ret, cor[0] = cv2.threshold(cor[0],0.001*cor[0].max(),255,0)
@@ -106,3 +159,9 @@ for path in dataset_path:
     
     corres = NCC(grayscale,corners)
     print (corres[2][0])
+
+    #for img in cor:
+     #   cv2.imshow("img", img)
+
+      #  if cv2.waitKey(0) & 0xff == 27:
+       #     cv2.destroyAllWindows()'''
